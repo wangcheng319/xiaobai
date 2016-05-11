9cc63f173e95c737f2caf718751538292bb57ade
wang.cheng <WangCheng@CHEN-PC>
2016-05-11 11:19:54 +0800

    框架搭建——引入okhttp 和glide

--- app/src/main/java/com/xiaobai/app/App.java
+++ app/src/main/java/com/xiaobai/app/App.java
@@ -2,8 +2,23 @@ package com.xiaobai.app;
 
 import android.app.Application;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.zhy.http.okhttp.OkHttpUtils;
+
 /**
  * Created by wangc on 2016/5/11.
  */
-public class App extends Application{
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        OkHttpClient client =
+                OkHttpUtils.getInstance().getOkHttpClient();
+
+        Request request = new Request.Builder().addHeader("","").build();
+
+    }
 }
