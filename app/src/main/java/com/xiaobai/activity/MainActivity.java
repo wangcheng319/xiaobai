8e1af44e8d3af9d5486a391cdcdac457dcca58b9
wang.cheng <WangCheng@CHEN-PC>
2016-05-11 10:42:50 +0800

    框架搭建

--- app/src/main/java/com/xiaobai/activity/MainActivity.java
+++ app/src/main/java/com/xiaobai/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.xiaobai.myapplication;
+package com.xiaobai.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.xiaobai.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/xiaobai/app/App.java
@@ -0,0 +1,9 @@
+package com.xiaobai.app;
+
+import android.app.Application;
+
+/**
+ * Created by wangc on 2016/5/11.
+ */
+public class App extends Application{
+}
