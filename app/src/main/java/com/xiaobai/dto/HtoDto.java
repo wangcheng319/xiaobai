b169b2e5c285b09d7f6baf0c4b1b7698b1c9a032
Wangc <274281610@qq.com>
2016-05-14 20:04:58 +0800

    add network

---
+++ app/src/main/java/com/xiaobai/dto/HtoDto.java
@@ -0,0 +1,22 @@
+package com.xiaobai.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by Wangc on 2016/5/14.
+ */
+
+public class HtoDto implements Serializable {
+
+    public String Context;
+    public List<String> urls;
+    public String Type;
+    public String NickName;
+    public String CreateTime;
+    public String Id;
+    public String Label;
+    public String UserId;
+    public String url;
+
+}
--- app/src/main/java/com/xiaobai/fragment/FindFragment.java
+++ app/src/main/java/com/xiaobai/fragment/FindFragment.java
@@ -3,21 +3,31 @@ package com.xiaobai.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.GestureDetector;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 import com.xiaobai.adapter.FindAdapter;
+import com.xiaobai.dto.HtoDto;
 import com.xiaobai.myapplication.R;
 
-import java.util.ArrayList;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 发现
@@ -33,7 +43,8 @@ public class FindFragment extends Fragment {
     private View rootView;
     private RecyclerView mList;
     private FindAdapter findAdapter;
-    private List<String> mDatas;
+    private List<HtoDto> mDatas;
+    public String url = "http://janhuu.imwork.net:30319/qianyuApp/requestservices.action";
 
 
     public FindFragment() {
@@ -68,27 +79,76 @@ public class FindFragment extends Fragment {
         initView();
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        setNetWork();
+    }
+
+    private void setNetWork() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    try {
+                        post(url);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * 网络设置
+     */
+
+    public void post(String url) throws IOException, JSONException {
+        OkHttpClient mOkHttpClient = new OkHttpClient();
+        mOkHttpClient.setConnectTimeout(30, TimeUnit.SECONDS);
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("CmdId", "queryHotRecord")
+                .add("Goal", "record")
+                .add("c_pageCount", "10")
+                .add("c_currentPage", "1")
+                .add("Version", "01")
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build();
+
+        Response response = mOkHttpClient.newCall(request).execute();
+        if (response.isSuccessful()) {
+            String result = response.body().string();
+            Log.e("return", result);
+            System.out.print(result);
+            Gson gson = new Gson();
+
+            JSONObject jsonObject = new JSONObject(result);
+            String data = jsonObject.getString("data");
+            java.lang.reflect.Type type2 = new TypeToken<List<HtoDto>>() {
+            }.getType();
+            mDatas = gson.fromJson(data,type2);
+            findAdapter = new FindAdapter(getActivity(), mDatas);
+            mList.setAdapter(findAdapter);
+
+        } else {
+            throw new IOException("Unexpected code " + response);
+        }
+
+    }
+
     private void initView() {
 
-        initData();
         mList = (RecyclerView) rootView.findViewById(R.id.find_list);
         mList.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mList.setAdapter(findAdapter = new FindAdapter(getActivity(), mDatas));
-
-        findAdapter.setmOnItemClickListener(new FindAdapter.OnRecyclerViewItemClickListener() {
-            @Override
-            public void onItemClick(View view, String data) {
-                Toast.makeText(getActivity(),data,Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        findAdapter = new FindAdapter(getActivity(), mDatas);
+        mList.setAdapter(findAdapter);
     }
 
-
-    protected void initData() {
-        mDatas = new ArrayList<String>();
-        for (int i = 'A'; i < 'z'; i++) {
-            mDatas.add("" + (char) i);
-        }
-    }
 }
