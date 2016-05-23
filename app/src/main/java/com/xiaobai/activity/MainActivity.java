60abdf662142b571112832814bf0be860e1c7511
wang.cheng <WangCheng@CHEN-PC>
2016-05-23 11:36:49 +0800

    修改列表布局

--- app/src/main/java/com/xiaobai/activity/MainActivity.java
+++ app/src/main/java/com/xiaobai/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -52,6 +53,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         initView();
