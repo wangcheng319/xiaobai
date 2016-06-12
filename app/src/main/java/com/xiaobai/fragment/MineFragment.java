1b99d6fc44b97cca008135ffa0f9da66a6280470
Wangc <274281610@qq.com>
2016-06-12 17:32:48 +0800

    新增个人中心

--- app/src/main/java/com/xiaobai/fragment/MineFragment.java
+++ app/src/main/java/com/xiaobai/fragment/MineFragment.java
@@ -3,11 +3,13 @@ package com.xiaobai.fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.ecloud.pulltozoomview.PullToZoomScrollViewEx;
 import com.xiaobai.application.R;
 
 
@@ -16,6 +18,7 @@ import com.xiaobai.application.R;
  */
 public class MineFragment extends BaseFragment {
     private View rootView;
+    private PullToZoomScrollViewEx scrollView;
 
 
     public MineFragment() {
@@ -33,6 +36,23 @@ public class MineFragment extends BaseFragment {
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_mine, container, false);
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        loadViewForCode();
+        scrollView = (PullToZoomScrollViewEx) rootView.findViewById(R.id.mine_scrollview);
+    }
+
+    private void loadViewForCode() {
+
+        View headView = LayoutInflater.from(getActivity()).inflate(R.layout.view_more_list_item, null, false);
+        View zoomView = LayoutInflater.from(getActivity()).inflate(R.layout.activity_login, null, false);
+        View contentView = LayoutInflater.from(getActivity()).inflate(R.layout.view_title, null, false);
+        scrollView.setHeaderView(headView);
+        scrollView.setZoomView(zoomView);
+        scrollView.setScrollContentView(contentView);
     }
 
     @Override
